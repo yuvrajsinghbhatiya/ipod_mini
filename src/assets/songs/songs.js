01aad1a6ed20a4db33f15229c16921e893884d08
Yuvraj Singh Bhatiya <111226345+yvrjbhatiya@users.noreply.github.com>
2023-09-13 17:59:05 +0530

    Add files via upload

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./play.mp3";
+import music2 from "./cry.mp3";
+import music3 from "./undefeated.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
